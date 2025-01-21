ebc8a44565d679f6138fef948a244e74e94257bd
andrewhoncharenko <goncharenkoandrew5643@gmail.com>
2025-01-21 22:37:53 +0200

    modal dialog

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,22 @@
+import { useRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
+import Button from "../Button";
+
+export default function Modal({children, buttonCaption,  ref}) {
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        };
+    });
+
+    return createPortal(<dialog className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md" ref={dialog}>
+        {children}
+        <form className="mt-4 text-right" method="dialog">
+            <Button>{buttonCaption}</Button>
+        </form>
+    </dialog>, document.getElementById("modal-root"));
+}
